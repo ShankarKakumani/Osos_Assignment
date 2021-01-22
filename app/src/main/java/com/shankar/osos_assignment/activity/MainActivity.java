7575a004e714911fde5d26fad5becf79397d6793
Shankar Chowdary <shankarchowdary7545@gmail.com>
2021-01-22 07:51:33 +0530

    Latest

--- app/src/main/java/com/shankar/osos_assignment/activity/MainActivity.java
+++ app/src/main/java/com/shankar/osos_assignment/activity/MainActivity.java
@@ -19,6 +19,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.shankar.customtoast.Toasty;
 import com.shankar.osos_assignment.R;
 import com.shankar.osos_assignment.adapter.ImageInterface;
 import com.shankar.osos_assignment.adapter.ParentAdapter;
@@ -115,11 +116,24 @@ public class MainActivity extends AppCompatActivity implements ImageInterface {
 
         } else {
 
-            //Intent intent = new Intent(Intent.ACTION_GET_CONTENT);  For File Manager
-            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-            //intent.setType("image/*");
-            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-            startActivityForResult(Intent.createChooser(intent, "Pictures: "), PICK_IMAGE);
+            if(isPackageInstalled())
+            {
+                //Will run if google photos installed
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                //intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                startActivityForResult(Intent.createChooser(intent, "Pictures: "), PICK_IMAGE);
+            }
+            else
+            {
+                //Opens file manager
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                startActivityForResult(Intent.createChooser(intent, "Pictures: "), PICK_IMAGE);
+
+            }
+
 
         }
     }
@@ -139,6 +153,18 @@ public class MainActivity extends AppCompatActivity implements ImageInterface {
 
                 }
             }
+        }
+    }
+
+
+    private boolean isPackageInstalled() {
+        try {
+
+            PackageManager packageManager = getPackageManager();
+            packageManager.getPackageInfo("com.google.android.apps.photos", 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
         }
     }
 
